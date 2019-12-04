ab84dd98c4aab15f7c13a2e1b3883f537151cf4a
alexspringer <32534256+alexspringer@users.noreply.github.com>
2019-12-04 01:08:12 -0800

    finished functionality of a single room. will update for multiple room support tomorrow :+1:

--- irc-client/src/App.js
+++ irc-client/src/App.js
@@ -1,27 +1,40 @@
 import React, { Component } from "react";
 import socketIOClient from "socket.io-client";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      endpoint: "http://127.0.0.1:4001",
+      name: "",
+      socket: socketIOClient("http://localhost:4001")
     };
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  appendMessage(message){
+  handleSubmit(event) {
+    event.preventDefault();
+    const messageInput = document.getElementById("message-input");
+    var message = messageInput.value;
+    this.appendMessage(`You: ${message}`);
+    messageInput.value = "";
+    this.state.socket.emit("send-message", message);
+  }
+
+  appendMessage(message) {
     const messageContainer = document.getElementById("message-container");
-    const newMessage = document.createElement('div');
+    const newMessage = document.createElement("div");
     newMessage.innerText = message;
     messageContainer.append(newMessage);
   }
 
   //Appends a user to the list of active users.
-  appendUser(user){
-    if(document.getElementById(user) != null){
+  appendUser(user) {
+    if (document.getElementById(user) != null) {
       return; //This user is already an active user.
     }
     const userContainer = document.getElementById("user-container");
-    const newUser = document.createElement('div');
+    const newUser = document.createElement("div");
     newUser.innerText = user;
     newUser.id = user; //set the id to the user so we can easily remove when the user dc
     userContainer.append(newUser);
@@ -29,42 +42,83 @@ class App extends Component {
 
   //When a user disconnect, we emit the user who disconencted to the client.
   //We use that information to remove the user from the list of active users.
-  removeUser(user){
+  removeUser(user) {
     const userContainer = document.getElementById("user-container");
     const userToRemove = document.getElementById(user);
-    const throwAway = userContainer.removeChild(userToRemove);
+    if (!userToRemove) return;
+    userContainer.removeChild(userToRemove);
   }
 
-  newUser(socket){
+  newUser(socket) {
     const name = prompt("Enter your name please:");
+    this.setState({ name: name });
     this.appendMessage("You joined!");
     this.appendUser(name);
-    socket.emit('new-user', name);
+    socket.on("active-users", userList => {
+      console.log(userList);
+      if (userList) {
+        let i = 0;
+        for (i = 0; i < userList.length; i++) {
+          this.appendUser(userList[i]);
+        }
+      }
+    });
+
+    socket.emit("new-user", name);
   }
 
   componentDidMount() {
-    const { endpoint } = this.state;
-    const socket = socketIOClient(endpoint);
-    this.newUser(socket);
+    //const { endpoint } = this.state;
+    //const socket = socketIOClient(endpoint);
+    this.newUser(this.state.socket);
 
-    socket.on('user-connected', name =>{
-      this.appendMessage(`${name} connected.`);
+    this.state.socket.on("chat-message", data => {
+      this.appendMessage(`${data.name}: ${data.message}`);
     });
 
-    socket.on('update-active', user => {
-      console.log(user);
-      this.appendUser(user);
-    })
-    socket.on('user-disconnected', name =>{
+    this.state.socket.on("user-connected", name => {
+      this.appendMessage(`${name} connected.`);
+      this.appendUser(name);
+    });
+
+    this.state.socket.on("user-disconnected", name => {
       this.appendMessage(`${name} disconnected.`);
       this.removeUser(name);
     });
   }
   render() {
     return (
-      <div>
-        <div id="message-container"></div>
-        <div id="user-container"></div>
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-3"></div>
+          <div class="col-6">
+            <div class="container break-word" id="message-container">
+              <h6 class="text-center"> Message Container</h6>
+            </div>
+          </div>
+          <div class="col-3">
+            <div class="container" id="user-container">
+              <h6 class="text-center">User Container</h6>
+            </div>
+          </div>
+        </div>
+        <div class="row justify-content-center fixed-bottom mb-5">
+          <div class="col-6">
+            <form onSubmit={this.handleSubmit}>
+              <div class="input-group">
+                <input
+                  class="form-control"
+                  type="text"
+                  id="message-input"
+                  name="message-input"
+                />
+                <div class="input-group-append">
+                  <button type="submit">Send</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
--- server.js
+++ server.js
@@ -1,37 +1,39 @@
-const express = require('express')
-const http = require('http')
-const socketIO = require('socket.io')
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
 
 //localhost port
-const port = 4001
+const port = 4001;
 
-const app = express()
+const app = express();
 
 //server instance
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 // This creates our socket using the instance of the server
-const io = socketIO(server)
+const io = socketIO(server);
 
-users = {}
+users = {};
 
-io.on('connection', socket => {
-  console.log('New client connected');
-  Object.keys(users).forEach(function(item){
+io.on("connection", socket => {
+  console.log("New client connected");
+  var userList = Object.values(users);
+  socket.emit("active-users", userList);
 
-        socket.broadcast.emit('update-active', users[item]);
+  socket.on("new-user", name => {
+    users[socket.id] = name;
+    socket.broadcast.emit("user-connected", name);
+  });
+
+  socket.on("send-message", message =>{
+    socket.broadcast.emit("chat-message", {name: users[socket.id], message: message});
   })
-  
-  socket.on('new-user', (name) => {
-      users[socket.id] = name;
-      socket.broadcast.emit('user-connected', name);
-  })
-  
-  socket.on('disconnect', () => {
-    console.log('user disconnected')
-    socket.broadcast.emit('user-disconnected', users[socket.id]);
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+    socket.broadcast.emit("user-disconnected", users[socket.id]);
     delete users[socket.id];
-  })
-})
+  });
+});
 
-server.listen(port, () => console.log(`Listening on port ${port}`))
+server.listen(port, () => console.log(`Listening on port ${port}`));
